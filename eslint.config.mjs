9526ae133536156d9a9ed99bb9dd9ffc87706a26
zhy2on <52701529+zhy2on@users.noreply.github.com>
2024-08-05 16:16:57 +0900

    chore: eslint props 검사 해제

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,15 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    ...pluginReact.configs.flat.recommended,
+    rules: {
+      ...pluginReact.configs.flat.recommended.rules,
+      "react/prop-types": "off"  // 'react/prop-types' 규칙을 비활성화
+    }
+  },
 ];
